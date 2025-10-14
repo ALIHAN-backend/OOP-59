e646894fb732d1b607684d608e469bf7bd3cb4bc
ALIHAN-backend <alihansatylganov1@gmail.com>
2025-10-14 20:16:36 +0600

    Домашнее задание 4: работа с внешними библиотеками

---
+++ HomeWork/HomeWork_4.py
@@ -0,0 +1,5 @@
+import pyfiglet
+
+# Создаем ASCII-арт из текста
+ascii_banner = pyfiglet.figlet_format("Hello, Python!")
+print(ascii_banner)
---
+++ Lesson/Lesson_5.py
@@ -0,0 +1,96 @@
+# class Test:
+#      # Магический метод
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def __str__(self):
+#        return self.name
+#
+# class MyInt:
+#     def __init__(self, value):
+#          self.value = value
+#
+#     def __str__(self):
+#        return self.value
+#
+# test3 = MyInt(123)
+# test4 = 123
+#
+# print(test4)
+
+# class Money:
+#
+#     def __init__(self, amount, currency):
+#         self.amount = amount
+#         self.currency = currency
+#
+#     #                +
+#     def __add__(self, other):
+#         if self.currency == other.currency:
+#             print(self.amount + other.amount)
+#         else:
+#             print(f'Currency error')
+#         print(self.currency)
+#         print(other.currency)
+#         print(self.amount + other.amount)
+#     #                <
+#     def __lt__(self, other):
+#         print(self.amount)
+#         print(other.amount)
+#     #                >
+#     def __gt__(self, other):
+#         print(self.amount)
+#         print(other.amount)
+#
+# SOM = Money(100, 'SOM')
+#
+# USD = Money(100, 'USD')
+#
+# new_obj = SOM > USD
+
+# class MyList:
+#
+#     def __init__(self, value: list):
+#         self.value = value
+#
+#   def __new__(cls, *args, **kwargs):
+#
+#   def __setitem__(self, key, value):
+#         print(key) = [1]
+#         print(value) = 123
+#
+#   def __getitem__(self, item):
+#         print(item)
+#
+#   def __del__(self):
+#
+# test2 = MyList([1,23,4,56,7])
+# test2[1] =  123
+
+class Test:
+    #  Атрибута класса
+    name = "Class Name"
+    def __init__(self, value):
+    # Атрибута экземпляра класса
+     self.value = value
+#
+#   def test(self):
+#        pass
+#
+#   def __test2(self):
+#       pass
+
+    def _just_method(self):
+        print(f"{self.value} я все")
+
+    @staticmethod
+    def static_method():
+        print("я все")
+
+    @classmethod
+    def class_method(cls):
+        print(cls.name)
+
+# objects = Test("test")
+# Test.static_method()
+# Test._just_method()
